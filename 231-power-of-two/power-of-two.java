5e1cd5332170a6f93ca5cc35e9c7d4b2abda61d6
Kartikeya-Ambare <163032422+Kartikeya-Ambare@users.noreply.github.com>
2025-06-11 20:22:55 +0530

    Time: 1 ms (36.40%) | Memory: 41 MB (50.19%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -7,33 +7,6 @@ class Solution {
             n /= 2;
         }
         return n == 1;
-        /*
-        if( n == 1 || n == 2){
-            return true;
-        }
-        else{
-            int new_val=n;
-            String nlen=String.valueOf(new_val);
-            while(nlen.length()>=2){
-                new_val=(int) Math.round(Math.sqrt(new_val));
-                nlen=String.valueOf(new_val);
-            }
-            if(new_val == 2 || new_val == 4 || new_val==8){
-                return true;
-            }
-            else{
-                return false;
-            }*/
-            /*int curr_num = 2, input_num=n;
-            while(curr_num<=input_num){
-                curr_num=curr_num*2;
-                input_num=input_num/2;
-                if(curr_num == input_num){
-                    return true;
-                }
-                
-            }
-            return false;*/
         
     }
 }
