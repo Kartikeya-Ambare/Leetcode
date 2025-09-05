79e8522c0a76d4ca7cf01b19fc479c209685bccd
Kartikeya-Ambare <163032422+Kartikeya-Ambare@users.noreply.github.com>
2025-09-06 00:05:12 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (44.55%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.java
+++ 782-jewels-and-stones/jewels-and-stones.java
@@ -1,9 +1,5 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        // if(jewels.length()<stones.length())
-        //     return count_same(jewels,stones);
-        // else
-        //     return count_same(stones,jewels);
         int sum = 0;
         for(int i =0;i<jewels.length();i++){
             for(int j =0;j<stones.length();j++)
@@ -12,15 +8,5 @@ class Solution {
                 }
         }
         return sum;
-    }
-    public int count_same(String minn,String maxx){
-        int sum = 0;
-        for(int i =0;i<minn.length();i++){
-            for(int j =0;j<maxx.length();j++)
-                if(minn.charAt(i) == maxx.charAt(j)){
-                    sum++;
-                }
-        }
-        return sum;
-    }
+}
 }
