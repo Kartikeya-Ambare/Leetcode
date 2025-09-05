d82f79af16f4fadd509eee09a4dda607f0b07ace
Kartikeya-Ambare <163032422+Kartikeya-Ambare@users.noreply.github.com>
2025-09-06 00:04:41 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (62.85%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.java
+++ 782-jewels-and-stones/jewels-and-stones.java
@@ -1,9 +1,17 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        if(jewels.length()<stones.length())
-            return count_same(jewels,stones);
-        else
-            return count_same(stones,jewels);
+        // if(jewels.length()<stones.length())
+        //     return count_same(jewels,stones);
+        // else
+        //     return count_same(stones,jewels);
+        int sum = 0;
+        for(int i =0;i<jewels.length();i++){
+            for(int j =0;j<stones.length();j++)
+                if(jewels.charAt(i) == stones.charAt(j)){
+                    sum++;
+                }
+        }
+        return sum;
     }
     public int count_same(String minn,String maxx){
         int sum = 0;
