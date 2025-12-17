ae3f7d8a3c57dc0ff4c6dc484ba34bda65be93c1
Kartikeya-Ambare <163032422+Kartikeya-Ambare@users.noreply.github.com>
2025-12-17 23:17:40 +0530

    Time: 1 ms (99.89%) | Memory: 46.8 MB (45.66%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -3,9 +3,10 @@ class Solution {
         if(nums.length == 1){
             return nums[0];
         }
-        int uniqNum = 0;
+        int UniqueNum = 0;
         for (int idx : nums) {
-            uniqNum ^= idx;
-        } return uniqNum; 
+            UniqueNum ^= idx;
+        } 
+        return UniqueNum; 
     }
 }
