29344a63c68eb4bdb896c408b1995397d55f56eb
Kartikeya-Ambare <163032422+Kartikeya-Ambare@users.noreply.github.com>
2026-01-12 22:58:28 +0530

    Time: 74 ms (82.48%) | Memory: 134.8 MB (22.13%) - LeetSync

--- 307-range-sum-query-mutable/range-sum-query-mutable.java
+++ 307-range-sum-query-mutable/range-sum-query-mutable.java
@@ -15,58 +15,98 @@ class NumArray {
     //     }
     //     return sum;
     // }
-    int[] tree;
+    int[] nums;
+    int[] bit;
     int n;
 
     public NumArray(int[] nums) {
-        if (nums.length > 0) {
-            n = nums.length;
-            tree = new int[4 * n];
-            buildTree(nums, 0, 0, n - 1);
+        this.nums = nums;
+        n = nums.length;
+        bit = new int[n+1];
+
+        for(int i = 0; i < n; i++) {
+            updateBit(i+1, nums[i]);
         }
     }
 
-    private void buildTree(int[] nums, int treeIndex, int start, int end) {
-        if (start == end) {
-            tree[treeIndex] = nums[start];
-            return;
+    private void updateBit(int i, int delta) {
+        while(i <= n) {
+            bit[i] += delta;
+            i += i & -i;
         }
-        int mid = start + (end - start) / 2;
-        buildTree(nums, 2 * treeIndex + 1, start, mid);
-        buildTree(nums, 2 * treeIndex + 2, mid + 1, end);
-        tree[treeIndex] = tree[2 * treeIndex + 1] + tree[2 * treeIndex + 2];
     }
 
     public void update(int index, int val) {
-        updateTree(0, 0, n - 1, index, val);
+        int delta = val - nums[index];
+        nums[index] = val;
+        updateBit(index + 1, delta);
     }
 
-    private void updateTree(int treeIndex, int start, int end, int arrIndex, int val) {
-        if (start == end) {
-            tree[treeIndex] = val;
-            return;
+    private int query(int i) {
+        int sum = 0;
+        while(i > 0) {
+            sum += bit[i];
+            i -= i & -i;
         }
-        int mid = start + (end - start) / 2;
-        if (arrIndex <= mid) {
-            updateTree(2 * treeIndex + 1, start, mid, arrIndex, val);
-        } else {
-            updateTree(2 * treeIndex + 2, mid + 1, end, arrIndex, val);
-        }
-        tree[treeIndex] = tree[2 * treeIndex + 1] + tree[2 * treeIndex + 2];
+
+        return sum;
     }
 
     public int sumRange(int left, int right) {
-        return queryTree(0, 0, n - 1, left, right);
+        return query(right + 1) - query(left);
     }
+    // int[] tree;
+    // int n;
 
-    private int queryTree(int treeIndex, int start, int end, int queryLeft, int queryRight) {
-        if (end < queryLeft || start > queryRight) return 0;
-        if (queryLeft <= start && end <= queryRight) return tree[treeIndex];
-        int mid = start + (end - start) / 2;
-        int leftSum = queryTree(2 * treeIndex + 1, start, mid, queryLeft, queryRight);
-        int rightSum = queryTree(2 * treeIndex + 2, mid + 1, end, queryLeft, queryRight);
-        return leftSum + rightSum;
-    }
+    // public NumArray(int[] nums) {
+    //     if (nums.length > 0) {
+    //         n = nums.length;
+    //         tree = new int[4 * n];
+    //         buildTree(nums, 0, 0, n - 1);
+    //     }
+    // }
+
+    // private void buildTree(int[] nums, int treeIndex, int start, int end) {
+    //     if (start == end) {
+    //         tree[treeIndex] = nums[start];
+    //         return;
+    //     }
+    //     int mid = start + (end - start) / 2;
+    //     buildTree(nums, 2 * treeIndex + 1, start, mid);
+    //     buildTree(nums, 2 * treeIndex + 2, mid + 1, end);
+    //     tree[treeIndex] = tree[2 * treeIndex + 1] + tree[2 * treeIndex + 2];
+    // }
+
+    // public void update(int index, int val) {
+    //     updateTree(0, 0, n - 1, index, val);
+    // }
+
+    // private void updateTree(int treeIndex, int start, int end, int arrIndex, int val) {
+    //     if (start == end) {
+    //         tree[treeIndex] = val;
+    //         return;
+    //     }
+    //     int mid = start + (end - start) / 2;
+    //     if (arrIndex <= mid) {
+    //         updateTree(2 * treeIndex + 1, start, mid, arrIndex, val);
+    //     } else {
+    //         updateTree(2 * treeIndex + 2, mid + 1, end, arrIndex, val);
+    //     }
+    //     tree[treeIndex] = tree[2 * treeIndex + 1] + tree[2 * treeIndex + 2];
+    // }
+
+    // public int sumRange(int left, int right) {
+    //     return queryTree(0, 0, n - 1, left, right);
+    // }
+
+    // private int queryTree(int treeIndex, int start, int end, int queryLeft, int queryRight) {
+    //     if (end < queryLeft || start > queryRight) return 0;
+    //     if (queryLeft <= start && end <= queryRight) return tree[treeIndex];
+    //     int mid = start + (end - start) / 2;
+    //     int leftSum = queryTree(2 * treeIndex + 1, start, mid, queryLeft, queryRight);
+    //     int rightSum = queryTree(2 * treeIndex + 2, mid + 1, end, queryLeft, queryRight);
+    //     return leftSum + rightSum;
+    // }
 }
 
 /**
