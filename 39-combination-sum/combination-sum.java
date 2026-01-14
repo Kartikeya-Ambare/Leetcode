765861ba8a8c4a6cafa66c94cf32d330ee069bf9
Kartikeya-Ambare <163032422+Kartikeya-Ambare@users.noreply.github.com>
2026-01-14 22:24:32 +0530

    Time: 2 ms (95.07%) | Memory: 45.6 MB (85.97%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,20 +1,40 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] nums, int target) {
-    List<List<Integer>> list = new ArrayList<>();
-    Arrays.sort(nums);
-    backtrack(list, new ArrayList<>(), nums, target, 0);
-    return list;
-}
+//     public List<List<Integer>> combinationSum(int[] nums, int target) {
+//     List<List<Integer>> list = new ArrayList<>();
+//     Arrays.sort(nums);
+//     backtrack(list, new ArrayList<>(), nums, target, 0);
+//     return list;
+// }
 
-private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
-    if(remain < 0) return;
-    else if(remain == 0) list.add(new ArrayList<>(tempList));
-    else{ 
-        for(int i = start; i < nums.length; i++){
-            tempList.add(nums[i]);
-            backtrack(list, tempList, nums, remain - nums[i], i); 
-            tempList.remove(tempList.size() - 1);
+// private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, int remain, int start){
+//     if(remain < 0) return;
+//     else if(remain == 0) list.add(new ArrayList<>(tempList));
+//     else{ 
+//         for(int i = start; i < nums.length; i++){
+//             tempList.add(nums[i]);
+//             backtrack(list, tempList, nums, remain - nums[i], i); 
+//             tempList.remove(tempList.size() - 1);
+//         }
+//     }
+// }
+public static void findCom(int[] candidates, int target, int ind,List<List<Integer>> ans, List<Integer> ds){
+        if(ind==candidates.length){
+            if(target==0){
+                ans.add(new ArrayList<>(ds));
+            }
+            return;
         }
+        if(candidates[ind]<=target){
+            ds.add(candidates[ind]);
+            findCom(candidates,target-candidates[ind],ind,ans,ds);
+            ds.remove(ds.size()-1);
+        }
+        findCom(candidates,target,ind+1,ans,ds);
     }
-}
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans=new ArrayList<>();
+        List<Integer> ds=new ArrayList<>();
+        findCom(candidates,target,0,ans,ds);
+        return ans;
+    }
 }
