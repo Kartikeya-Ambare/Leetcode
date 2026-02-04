f014aa310cc89ab88d782336457901649391406e
Kartikeya-Ambare <163032422+Kartikeya-Ambare@users.noreply.github.com>
2026-02-04 23:26:50 +0530

    Time: 6 ms (26.11%) | Memory: 43.5 MB (39.85%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -1,22 +1,29 @@
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
-       int m = s1.length(), n = s2.length(), l = s3.length();
-        if (m + n != l) return false;
+       if (s1.length() + s2.length() != s3.length())
+            return false;
 
-        boolean[] dp = new boolean[n + 1];
-        dp[0] = true;
+        boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
 
-        for (int j = 1; j <= n; ++j) {
-            dp[j] = dp[j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1);
+        dp[0][0] = true;
+
+        for (int i = 1; i < dp.length; i++) {
+            dp[i][0] = dp[i - 1][0] && s1.charAt(i - 1) == s3.charAt(i - 1);
         }
 
-        for (int i = 1; i <= m; ++i) {
-            dp[0] = dp[0] && s1.charAt(i - 1) == s3.charAt(i - 1);
-            for (int j = 1; j <= n; ++j) {
-                dp[j] = (dp[j] && s1.charAt(i - 1) == s3.charAt(i + j - 1)) || (dp[j - 1] && s2.charAt(j - 1) == s3.charAt(i + j - 1));
+        for (int j = 1; j < dp[0].length; j++) {
+            dp[0][j] = dp[0][j - 1] && s2.charAt(j - 1) == s3.charAt(j - 1);
+        }
+
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+
+                char c = s3.charAt(i + j - 1);
+
+                dp[i][j] = (s1.charAt(i - 1) == c && dp[i - 1][j]) || (s2.charAt(j - 1) == c && dp[i][j - 1]);
             }
         }
-        
-        return dp[n]; 
+
+        return dp[dp.length - 1][dp[0].length - 1];
     }
 }
