b232b6d00eeacb6249c9d35272cbc38a5e71e7c8
Kartikeya-Ambare <163032422+Kartikeya-Ambare@users.noreply.github.com>
2026-02-18 19:08:56 +0530

    Time: 18 ms (51.85%) | Memory: 47.1 MB (59.88%) - LeetSync

---
+++ 839-short-encoding-of-words/short-encoding-of-words.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minimumLengthEncoding(String[] words) {
+        Set<String> set = new HashSet<>(Arrays.asList(words));
+        for (String word : words)
+            if (set.contains(word))
+                for (int i = 1; i < word.length(); i++) 
+                    set.remove(word.substring(i));
+        int ans = set.size();
+        for (String word : set) ans += word.length();
+        return ans;
+    }
+}
