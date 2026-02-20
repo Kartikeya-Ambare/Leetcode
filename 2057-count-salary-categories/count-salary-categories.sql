85d9e2ba355591bb0fccaf64aae3f6c69a511cf0
Kartikeya-Ambare <163032422+Kartikeya-Ambare@users.noreply.github.com>
2026-02-20 20:14:58 +0530

    Time: 774 ms (14.06%) | Memory: 0B (100.00%) - LeetSync

--- 2057-count-salary-categories/count-salary-categories.sql
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -1,16 +1,35 @@
 /* Write your PL/SQL query statement below */
-SELECT 'Low Salary' AS category, COUNT(*) AS accounts_count 
+-- SELECT (CASE WHEN INCOME <20000 THEN 'Low Salary'
+--         WHEN INCOME between 20000 and 50000 then "Average Salary"
+--         When income>50000 then "High Salary" end) as Category,
+--         Count(Category)
+--         from accounts;
+
+-- SELECT 
+--     (CASE 
+--         WHEN INCOME < 20000 THEN 'Low Salary'
+--         WHEN INCOME BETWEEN 20000 AND 50000 THEN 'Average Salary'
+--         WHEN INCOME > 50000 THEN 'High Salary' 
+--     END) AS Category,
+--     COUNT(*) AS Category_Count
+-- FROM accounts
+-- GROUP BY 
+--     (CASE 
+--         WHEN INCOME < 20000 THEN 'Low Salary'
+--         WHEN INCOME BETWEEN 20000 AND 50000 THEN 'Average Salary'
+--         WHEN INCOME > 50000 THEN 'High Salary' 
+--     END);
+
+
+SELECT 'Low Salary' AS category, 
+       COUNT(CASE WHEN income < 20000 THEN 1 END) AS accounts_count
 FROM Accounts
-WHERE income < 20000
-
-UNION
-
-SELECT 'Average Salary' AS category, COUNT(*) AS accounts_count 
+UNION ALL
+SELECT 'Average Salary' AS category,
+       COUNT(CASE WHEN income BETWEEN 20000 AND 50000 THEN 1 END) AS accounts_count
 FROM Accounts
-WHERE income BETWEEN 20000 AND 50000
+UNION ALL
+SELECT 'High Salary' AS category,
+       COUNT(CASE WHEN income > 50000 THEN 1 END) AS accounts_count
+FROM Accounts;
 
-UNION
-
-SELECT 'High Salary' AS category, COUNT(*) AS accounts_count 
-FROM Accounts
-WHERE income > 50000
