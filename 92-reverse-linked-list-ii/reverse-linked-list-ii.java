d76e3734909c1d7fb26310b29beb8729e8133083
Kartikeya-Ambare <163032422+Kartikeya-Ambare@users.noreply.github.com>
2026-02-21 19:04:38 +0530

    Time: 5 ms (0.42%) | Memory: 43.1 MB (66.84%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.java
@@ -10,20 +10,49 @@
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode prev = dummy;
+        // ListNode dummy = new ListNode(0);
+        // dummy.next = head;
+        // ListNode prev = dummy;
         
-        for(int i = 0; i < left - 1; i++)
-            prev = prev.next;
+        // for(int i = 0; i < left - 1; i++)
+        //     prev = prev.next;
         
-        ListNode curr = prev.next;
-        for(int i = 0; i < right - left; i++){
-            ListNode forw = curr.next;
-            curr.next = forw.next;
-            forw.next = prev.next;
-            prev.next = forw;
+        // ListNode curr = prev.next;
+        // for(int i = 0; i < right - left; i++){
+        //     ListNode forw = curr.next;
+        //     curr.next = forw.next;
+        //     forw.next = prev.next;
+        //     prev.next = forw;
+        // }
+        // return dummy.next;
+
+        Stack<Integer> s = new Stack<>();
+        ListNode ptr = head;
+        int count = 1;
+        ListNode ptr2 = null;
+
+        while(ptr != null){
+            if(count == left){
+                ptr2 = ptr;
+                while(count <= right){
+                    s.push(ptr.val);
+                    count++;
+                    ptr = ptr.next;
+                }
+
+                while(!s.isEmpty()){
+                    int n = s.pop();
+                    System.out.print(n);
+                    ptr2.val = n;
+                    ptr2 = ptr2.next;
+                }
+                break;
+            }else{
+                ptr = ptr.next;
+                count++;
+            }
         }
-        return dummy.next;
+
+        return head;
     }
 }
