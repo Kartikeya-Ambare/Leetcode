7dc114c39863d1696485b3b66ae67992900412be
Kartikeya-Ambare <163032422+Kartikeya-Ambare@users.noreply.github.com>
2026-02-23 16:57:09 +0530

    Time: 7 ms (98.76%) | Memory: 46.2 MB (95.96%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -8,18 +8,40 @@
  * }
  */
 class Solution {
+    // public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+    //     if (root == null || root == p || root == q) {
+    //         return root;
+    //     }
+
+    //     TreeNode left = lowestCommonAncestor(root.left, p, q);
+    //     TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+    //     if (left != null && right != null) {
+    //         return root;
+    //     }
+
+    //     return left != null ? left : right;
+    // }
+
+    TreeNode ans;
+
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null || root == p || root == q) {
-            return root;
-        }
+        dfs(root, p, q);
+        return ans;
+    }
 
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
+    public boolean dfs(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return false;
 
-        if (left != null && right != null) {
-            return root;
-        }
+        boolean left = dfs(root.left, p, q);
+        boolean right = dfs(root.right, p, q);
 
-        return left != null ? left : right;
+        if (left && right) ans = root;
+        if (left && root == p || left && root == q) ans = root;
+        if (right && root == p || right && root == q) ans = root;
+
+        if (root == p || root == q) return true;
+
+        return left || right;
     }
 }
