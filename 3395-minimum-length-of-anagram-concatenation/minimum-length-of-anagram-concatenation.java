698583094f719ce05d805dbae53d4a30ebbfb81d
Kartikeya-Ambare <163032422+Kartikeya-Ambare@users.noreply.github.com>
2026-02-24 23:13:36 +0530

    Time: 132 ms (25.45%) | Memory: 47.8 MB (28.18%) - LeetSync

--- 3395-minimum-length-of-anagram-concatenation/minimum-length-of-anagram-concatenation.java
+++ 3395-minimum-length-of-anagram-concatenation/minimum-length-of-anagram-concatenation.java
@@ -1,30 +1,62 @@
 class Solution {
-    public int minAnagramLength(String s) {
-        for(int i=1;i<=s.length();i++){
-            if(s.length()%i==0 && isPossible(s,i)){
-                return i;
-            }
-        }
-        return -1;
-    }
+    // public int minAnagramLength(String s) {
+    //     for(int i=1;i<=s.length();i++){
+    //         if(s.length()%i==0 && isPossible(s,i)){
+    //             return i;
+    //         }
+    //     }
+    //     return -1;
+    // }
 
-    public boolean isPossible(String s, int length){
-        int [] first=new int [26];
-        for(int i=0;i<length;i++){
-            first[s.charAt(i)-'a']++;
-        }
+    // public boolean isPossible(String s, int length){
+    //     int [] first=new int [26];
+    //     for(int i=0;i<length;i++){
+    //         first[s.charAt(i)-'a']++;
+    //     }
 
-        for(int i=length;i<s.length();i+=length){
-            int [] thisOccurence =new int [26];
-            for(int j=i;j<i+length;j++){
-                thisOccurence[s.charAt(j)-'a']++;
-            }
-            for(int k=0;k<26;k++){
-                if(thisOccurence[k]!=first[k]){
-                    return false;
-                }
-            }
+    //     for(int i=length;i<s.length();i+=length){
+    //         int [] thisOccurence =new int [26];
+    //         for(int j=i;j<i+length;j++){
+    //             thisOccurence[s.charAt(j)-'a']++;
+    //         }
+    //         for(int k=0;k<26;k++){
+    //             if(thisOccurence[k]!=first[k]){
+    //                 return false;
+    //             }
+    //         }
            
+    //     }
+    //     return true;
+    // }
+
+
+    public int minAnagramLength(String s) {
+        int n = s.length();
+        for (int len = 1; len < n; len++) {
+            if (n % len != 0) {
+                continue;
+            }
+            if (isValid(s, len)) {
+                return len;
+            }
+        }
+        return n;
+    }
+    
+    private boolean isValid(String s, int len) {
+        int n = s.length();
+        char[] arr = s.substring(0, len).toCharArray();
+        Arrays.sort(arr);
+        String p = new String(arr);
+        for (int i = len; i + len <= n; i += len) {
+            char[] temp = new char[len];
+            for (int j = 0; j < len; j++) {
+                temp[j] = s.charAt(i + j);
+            }
+            Arrays.sort(temp);
+            if (!p.equals(new String(temp))) {
+                return false;
+            }
         }
         return true;
     }
