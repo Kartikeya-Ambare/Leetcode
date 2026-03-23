7cf3acb4f6a405da946cf9b35d85e8491192d7c9
Kartikeya Ambare <163032422+Kartikeya-Ambare@users.noreply.github.com>
2026-03-23 23:48:48 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (28.89%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,11 +1,26 @@
 class Solution {
     public int climbStairs(int n) {
-        ArrayList<Integer> lis = new ArrayList<>();
-        lis.add(1);
-        lis.add(2);
-        for(int i = 2 ;i<=n;i++){
-            lis.add(lis.get(i-1)+lis.get(i-2));
+        // ArrayList<Integer> lis = new ArrayList<>();
+        // lis.add(1);
+        // lis.add(2);
+        // for(int i = 2 ;i<=n;i++){
+        //     lis.add(lis.get(i-1)+lis.get(i-2));
+        // }
+        // return lis.get(n-1);
+        int dp[] = new int[n+1];
+        Arrays.fill(dp,-1);
+        return calc(dp,n);
+    }
+
+    public int calc(int dp[], int n){
+        if(n<=2){
+            return n;
         }
-        return lis.get(n-1);
+
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        dp[n] = calc(dp,n-1)+calc(dp,n-2);
+        return dp[n];
     }
 }
