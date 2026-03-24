1f4a3c9470cece73a7059ff5f6b694814acf481f
Kartikeya Ambare <163032422+Kartikeya-Ambare@users.noreply.github.com>
2026-03-24 23:50:54 +0530

    Time: 1 ms (38.61%) | Memory: 45.1 MB (18.02%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -6,24 +6,35 @@ class Solution {
     //     total+=Math.min(cost[i-1],cost[i-2]);
     //     }  
     // return total;
+        int n = cost.length;
         int dp[] = new int[cost.length+1];
         Arrays.fill(dp,-1);
-        return recur(cost.length,cost,dp);
-    }
+        dp[0]=0;
+        dp[1]=0;
 
-    public static int recur(int n, int arr[],int dp[]){
-        if(n ==1 || n==0){
-            dp[n]=0;
-        }
-        if(dp[n]!=-1){
-            return dp[n];
+        for(int i =2;i<dp.length;i++){
+            int stp1 = cost[i-1]+dp[i-1];
+            int stp2 = cost[i-2]+dp[i-2];
+            dp[i]=Math.min(stp1,stp2);
         }
 
-
-        int stp1 = arr[n-1]+recur(n-1,arr,dp);
-        int stp2 = arr[n-2]+recur(n-2,arr,dp);
-        dp[n]=Math.min(stp1,stp2);
-        return dp[n];
+        // return recur(cost.length,cost,dp);
+        return dp[cost.length];
     }
+
+    // public static int recur(int n, int arr[],int dp[]){
+    //     if(n ==1 || n==0){
+    //         dp[n]=0;
+    //     }
+    //     if(dp[n]!=-1){
+    //         return dp[n];
+    //     }
+
+
+        // int stp1 = arr[n-1]+recur(n-1,arr,dp);
+        // int stp2 = arr[n-2]+recur(n-2,arr,dp);
+        // dp[n]=Math.min(stp1,stp2);
+    //     return dp[n];
+    // }
     
 }
