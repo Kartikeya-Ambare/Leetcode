043cbe7e78a1eaeeb1318d8176858da0e5472c23
Kartikeya Ambare <163032422+Kartikeya-Ambare@users.noreply.github.com>
2026-08-18 23:36:25 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (75.90%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,35 +1,20 @@
 class Solution {
     public int rob(int[] nums) {
-        // int n = nums.length;
-        // if (n == 1) {
-        //     return nums[0];
-        // }
-        // int[] dp = new int[n];
-        // dp[0] = nums[0];
-        // dp[1] = Math.max(nums[0], nums[1]);
-        // for (int i = 2; i < n; i++) {
-        //     dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
-        // }
-        // return dp[n - 1];
-        int dp [] = new int[nums.length];
-        Arrays.fill(dp,-1);
-        return recur(nums,nums.length-1,dp);
+     Integer dp[] = new Integer[nums.length];
+
+     return solver(nums.length-1,nums,dp);   
     }
 
-    public static int recur(int nums[],int idx,int [] dp){
-        if(idx == 0){
-            return nums[0];
-        }
-        if(idx == -1){
-            return 0;
-        }
-        if(dp[idx]!=-1){
+    public static int solver(int idx,int nums[], Integer dp[]){
+        if(idx==0) return nums[0];
+        if(idx == -1) return 0;
+
+        if(dp[idx]!=null){
             return dp[idx];
         }
+        int pick = nums[idx] + solver(idx-2,nums,dp);
+        int nopick =solver(idx-1,nums,dp);
 
-        int pick = nums[idx]+recur(nums,idx-2,dp);
-        int nopick = 0 + recur(nums,idx-1,dp);
-        dp[idx] = Math.max(pick,nopick);
-        return dp[idx];
-    }   
+        return dp[idx] = Math.max(pick,nopick);
+    }
 }
