1b8fa0d7b3d679db71a2f66932751b27a54c6c84
Kartikeya Ambare <163032422+Kartikeya-Ambare@users.noreply.github.com>
2026-08-18 19:26:35 +0530

    Time: 28 ms (24.83%) | Memory: 46.8 MB (35.83%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,35 +1,25 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
+        if(amount == 0) return 0 ;
+
         int dp[][] = new int[coins.length][amount+1];
-        for(int i = 0 ; i<coins.length;i++){
-            for(int j = 0 ; j<amount+1;j++){
-                dp[i][j] = -1;
-            }
-            
-        }
-        int res = recur(coins,amount,coins.length-1,dp);
+        for(int x[]:dp) Arrays.fill(x,-1);
+        int res =solver(coins,amount,dp,coins.length-1);
         return res>=(int)(1e9)?-1:res;
     }
 
-    public int recur(int coins[], int amt, int idx,int dp[][]){
-        if(idx == 0){
-            if(amt % coins[idx] == 0){
-                dp[idx][amt] = amt/coins[idx];
-                return dp[idx][amt];
-            }
-            dp[idx][amt] = (int)(1e9);
-            return dp[idx][amt];
+    public static int solver(int coins[], int amt, int dp[][],int idx){
+        if(idx == 0) {
+            if(amt % coins[0] == 0) return amt / coins[0];
+            else return (int)1e9; 
         }
-        if(dp[idx][amt]!=-1){
-            return dp[idx][amt];
-        }
-        int pick = (int)(1e9);
-        if(amt>=coins[idx]){
-            pick = 1+ recur(coins,amt-coins[idx],idx,dp);
-        }
-        int nopick = recur(coins,amt,idx-1,dp);
-        dp[idx][amt]  = Math.min(pick,nopick);        
-        return dp[idx][amt];
-    }
 
+        if(dp[idx][amt]!=-1) return dp[idx][amt];
+        int pick = (int)(1e9);
+        int nopick = 0;
+        if(coins[idx]<=amt) pick = 1+solver(coins,amt-coins[idx],dp,idx);
+        nopick = solver(coins,amt,dp,idx-1);
+
+        return dp[idx][amt] = Math.min(pick,nopick);
+    }
 }
