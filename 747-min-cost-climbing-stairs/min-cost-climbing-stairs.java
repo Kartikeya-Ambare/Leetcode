0a8befe3893fc92e81e07f4de383afd0ec9de31b
Kartikeya Ambare <163032422+Kartikeya-Ambare@users.noreply.github.com>
2026-08-19 18:54:02 +0530

    Time: 1 ms (40.28%) | Memory: 45.1 MB (9.91%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.java
@@ -1,40 +1,20 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
-    // int min = Integer.MIN_VALUE;
-    // int total=0;
-    // for(int i =2;i<=cost.length;i++){
-    //     total+=Math.min(cost[i-1],cost[i-2]);
-    //     }  
-    // return total;
-        int n = cost.length;
-        int dp[] = new int[cost.length+1];
-        Arrays.fill(dp,-1);
-        dp[0]=0;
-        dp[1]=0;
+        Integer dp[] = new Integer[cost.length];
 
-        for(int i =2;i<dp.length;i++){
-            int stp1 = cost[i-1]+dp[i-1];
-            int stp2 = cost[i-2]+dp[i-2];
-            dp[i]=Math.min(stp1,stp2);
-        }
-
-        // return recur(cost.length,cost,dp);
-        return dp[cost.length];
+        int z = solver(cost,0,dp,cost.length);
+        int f = solver(cost,1,dp,cost.length);
+        return Math.min(z,f);
     }
 
-    // public static int recur(int n, int arr[],int dp[]){
-    //     if(n ==1 || n==0){
-    //         dp[n]=0;
-    //     }
-    //     if(dp[n]!=-1){
-    //         return dp[n];
-    //     }
+    public static int solver(int cost[], int idx, Integer dp[], int len){
+        if(idx >= len) return 0;
 
+        if(dp[idx]!=null) return dp[idx];
 
-        // int stp1 = arr[n-1]+recur(n-1,arr,dp);
-        // int stp2 = arr[n-2]+recur(n-2,arr,dp);
-        // dp[n]=Math.min(stp1,stp2);
-    //     return dp[n];
-    // }
-    
+        int one = cost[idx] + solver(cost,idx+1,dp,len);
+        int tow = cost[idx] + solver(cost,idx+2,dp,len);
+
+        return dp[idx] = Math.min(one,tow);
+    }
 }
